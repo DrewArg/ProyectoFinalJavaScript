1d2e1c112d435e1c516c6a43f315c8c34b19a72d
DrewArg <andresfabbiano5@gmail.com>
2022-01-23 01:08:56 -0300

    corregido error

--- script/incio.js
+++ script/incio.js
@@ -22,14 +22,17 @@ class Usuario {
 }
 
 const usuarios = [];
+const baseDatosUsuarios = localStorage.getItem("listaUsuarios");
 
-localStorage.setItem("listaUsuarios",JSON.stringify(usuarios));
+if (baseDatosUsuarios === null || baseDatosUsuarios.length === 0) {
+  localStorage.setItem("listaUsuarios", JSON.stringify(usuarios));
+}
 
 let btnIngreso = document.getElementById("btnIngreso");
-btnIngreso.addEventListener("click", validarUsuario);
+btnIngreso.addEventListener("click", reiniciarLocalStorage);
 
 let btnCrearCuenta = document.getElementById("btnCrearCuenta");
-btnCrearCuenta.addEventListener("click",crearUsuario)
+btnCrearCuenta.addEventListener("click", crearUsuario);
 
 let btnVerificar = document.getElementById("btnVerificar");
 btnVerificar.addEventListener("click", recorrerLocalStorage);
@@ -53,22 +56,23 @@ function validarUsuario(e) {
 }
 
 function crearUsuario(e) {
-    let usuarioActual = document.getElementById("logueoUsuario").value;
+  let usuarioActual = document.getElementById("logueoUsuario").value;
 
-    const listaUsuarios = JSON.parse(localStorage.getItem("listaUsuarios"));
-    const listaNueva = [];
+  const listaUsuarios = JSON.parse(localStorage.getItem("listaUsuarios"));
+  const listaNueva = [];
 
-    const usuarioEncontrado = listaUsuarios.find(usuario=>usuario.nombre === usuarioActual.toUpperCase());
+  const usuarioEncontrado = listaUsuarios.find(
+    (usuario) => usuario.nombre === usuarioActual.toUpperCase()
+  );
 
-    if(usuarioEncontrado !== undefined){
-        alert("Usuario ya existente");
-    }else{
-        for (const usuario of listaUsuarios) {
-            listaNueva.push(usuario);
-        }
-        listaNueva.push(new Usuario(usuarioActual))
-        alert("Usuario, " + usuarioActual + " agregado al sistema.")
-        localStorage.setItem("listaUsuarios",JSON.stringify(listaNueva));
+  if (usuarioEncontrado !== undefined) {
+    alert("Usuario ya existente");
+  } else {
+    for (const usuario of listaUsuarios) {
+      listaNueva.push(usuario);
     }
-
+    listaNueva.push(new Usuario(usuarioActual));
+    alert("Usuario, " + usuarioActual + " agregado al sistema.");
+    localStorage.setItem("listaUsuarios", JSON.stringify(listaNueva));
+  }
 }
